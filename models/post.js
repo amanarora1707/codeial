beaf4e96d523fb5dd5095e10db993573d6e4216d
AMAN <amanarora17077@gmail.com>
2021-01-13 03:11:15 +0530

    creating schema for comments

--- models/post.js
+++ models/post.js
@@ -8,7 +8,19 @@ const postSchema =new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,  //connecting post with user with userid
         ref:'User'  //reffering to user schema
-    }
+    },
+
+    //include the array of ids of comments in the post schema itself
+
+    comments:[
+        {
+            
+                type: mongoose.Schema.Types.ObjectId,
+                ref:'comment' 
+        }
+    ]
+
+
 },{
     timestamps:true
 });
