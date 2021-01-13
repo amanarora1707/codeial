b13ebccb855fc5107ba39c7e363f2dc96388402d
AMAN <amanarora17077@gmail.com>
2021-01-14 01:55:47 +0530

    deleting the post by authorised user

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 const Post =require('../models/post');
+const Comment =require('../models/comment');
+
 
 module.exports.create =function(req,res){
 
@@ -11,3 +13,21 @@ module.exports.create =function(req,res){
         return res.redirect('back');
     });
 }
+
+module.exports.destroy =function(req,res){
+    Post.findById(req.params.id ,function(err,post){
+        //.id means converting the object id to string
+        if(post.user ==req.user.id){   //authentication if person who created the post and person who clicked on delete are same then only allowed to delete
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+
+                return res.redirect('back');
+
+            });
+        }
+        else{
+            return res.redirect('back'); //if not authenticated then not allowed to delte
+        }
+    })
+    };
